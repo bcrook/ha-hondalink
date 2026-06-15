891fad23e6e759e303bf45bd1b8fe525938ccab4
Brian Crook <brian@Mac.localdomain>
2026-06-15 16:04:15 -0400

    apply redaction option to raw_vehcile_data

--- diagnostics.py
+++ diagnostics.py
@@ -5,7 +5,7 @@ from typing import Any
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DATA_COORDINATOR, DOMAIN
+from .const import CONF_DISABLE_REDACTION, DATA_COORDINATOR, DOMAIN
 from .coordinator import HondaLinkDataUpdateCoordinator
 
 
@@ -17,16 +17,25 @@ async def async_get_config_entry_diagnostics(
         DATA_COORDINATOR
     ]
 
+    disable_redaction = entry.options.get(CONF_DISABLE_REDACTION, False)
+
+    if disable_redaction:
+        entry_data = dict(entry.data)
+    else:
+        entry_data = {
+            k: v
+            for k, v in entry.data.items()
+            if k not in ["password", "access_token", "refresh_token", "client_reg_key", "pin"]
+        }
+
+    raw_vehicle_data = coordinator.data
+
     return {
         "entry": {
-            "data": {
-                k: v
-                for k, v in entry.data.items()
-                if k not in ["password", "access_token", "refresh_token", "client_reg_key"]
-            },
+            "data": entry_data,
             "options": dict(entry.options),
         },
-        "raw_vehicle_data": coordinator.data,
+        "raw_vehicle_data": raw_vehicle_data,
     }
 
 
